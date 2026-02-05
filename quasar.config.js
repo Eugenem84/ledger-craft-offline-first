58003ad081fc4bd63e2db60f48c96fb1789e4e2e
Eugene <medov-foto@mail.ru>
2026-02-05 21:07:43 +0300

    починил notify

--- quasar.config.js
+++ quasar.config.js
@@ -95,7 +95,9 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: [
+        'Notify'
+      ],
     },
 
     // animations: 'all', // --- includes all animations
